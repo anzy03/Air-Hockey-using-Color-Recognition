f57d29fdb8b4d03144952ca7c46a8e2f846984a9
ArnavSanghavi <arnav.sanghavi@gmail.com>
2019-04-05 13:43:38 +0530

    New pythonCord.
    Removed the error. Have maked in the file.

--- Assets/Scripts/pythonCord.cs
+++ Assets/Scripts/pythonCord.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class pythonCord : MonoBehaviour
 {
     // Start is called before the first frame update
+    System.Diagnostics.Process process = new System.Diagnostics.Process();
+    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
     void Start()
     {
         //var p = new System.Diagnostics.Process();
-        System.Diagnostics.Process process = new System.Diagnostics.Process();
-        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+        
         startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
         startInfo.FileName = "cmd.exe";
         startInfo.Arguments = "/python colorrec.py";
         process.StartInfo = startInfo;
+        //showed error about this.
+        process.StartInfo.UseShellExecute = false;
         process.StartInfo.RedirectStandardOutput = true;
-        process.StartInfo.RedirectStandardInput = true;
         process.Start();
         
         Debug.Log(process.StandardOutput.ReadToEnd());
@@ -24,6 +26,6 @@ public class pythonCord : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(process.StandardOutput.ReadToEnd());
     }
 }
